751fe340429a8354cbbeb42cfceb0d7eff2b0b5d
KatyKedi <katykat5bros@gmail.com>
2022-02-28 19:23:24 -0800

    first two fetches

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,48 @@
-let leftSearch = document.getElementsByClassName(".search-left-container");
-let rightSearch = document.getElementsByClassName(".search-right-container");
+var leftSearch = document.querySelector(".search-left-container");
+var rightSearch = document.querySelector(".search-right-container");
+var leftInputEl = document.querySelector('input[name="search-left"]');
 
-function getAPILeft() {
-  let requestURL = `https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/BCISTHEBEST?api_key=RGAPI-0561ded4-3020-4fec-a8c6-b33598f42810`;
-  fetch(requestURL).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (apileftData) {});
-      console.log(response);
-    }
-  });
-  
+var summoner = {
+    name: '',
+    id: '',
+    image: '',
+    wins: 0,
+    losses: 0,
+    winPercentage: ''
+};
+
+function getAPILeft(summonerName) {
+    var requestURL = `https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/${summonerName}?api_key=RGAPI-0561ded4-3020-4fec-a8c6-b33598f42810`;
+    fetch(requestURL).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (apiData) {
+                summoner.name = summonerName;
+                summoner.id = apiData.id;
+                secondFetch(summoner.id);
+            });
+        }
+    });
+};
+
+function secondFetch(id) {
+    var secondURL = `https://na1.api.riotgames.com/tft/league/v1/entries/by-summoner/${id}?api_key=RGAPI-0561ded4-3020-4fec-a8c6-b33598f42810`;
+    fetch(secondURL).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (apiData) {
+                summoner.image = '';
+                summoner.losses = apiData.losses;
+            });
+        }
+    });
 }
 
-getAPILeft();
+function printStats(event) {
+    event.preventDefault();
+    targetEl = event.target;
+    if (targetEl.matches(".search-left")) {
+        var summonerName = leftInputEl.value;
+        getAPILeft(summonerName);
+    }
+};
+
+leftSearch.addEventListener("click", printStats);
