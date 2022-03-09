7568c5a4bf5ffdae1eb8e9410bbc41ec36e85429
Aaron Weaver <atweaver7@gmail.com>
2022-03-08 23:31:29 -0800

    push

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,17 @@
 let leftSearch = document.getElementsByClassName(".search-left-container");
 let rightSearch = document.getElementsByClassName(".search-right-container");
+const nameLeft = document.getElementById("#sumName")
 let summonerAccountID1 = [];
 let summonerID1 = [];
 let summonerAccountID2 = [];
 let summonerID2 = [];
 let puuid1 = [];
+let leftName = [];
 
 
-
+// Api request left box begin
 function getAPILeft() {
-  let requestURL = `https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/n4a17?api_key=RGAPI-0561ded4-3020-4fec-a8c6-b33598f42810`;
+  let requestURL = `https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/BCISTHEBEST?api_key=RGAPI-0561ded4-3020-4fec-a8c6-b33598f42810`;
   fetch(requestURL).then(function (response) {
     if (response.ok) {
       response.json().then(function (apileftData) {
@@ -18,25 +20,39 @@ function getAPILeft() {
         puuid1.push(apileftData.puuid)
         localStorage.setItem("ID", summonerID1);
         getWins();
-       console.log(apileftData)
-       console.log(puuid1)
+      //  console.log(apileftData)
+      //  console.log(puuid1)
       });
     }
   });
 }
 
 function getWins(apileftData) {
-    let requestURL = `https://na1.api.riotgames.com/tft/league/v1/entries/by-summoner/${summonerAccountID1}/ids?api_key=RGAPI-0561ded4-3020-4fec-a8c6-b33598f42810`;
+    let requestURL = `https://na1.api.riotgames.com/tft/league/v1/entries/by-summoner/${summonerID1}?api_key=RGAPI-0561ded4-3020-4fec-a8c6-b33598f42810`;
   fetch(requestURL).then(function (response) {
     if (response.ok) {
       response.json().then(function (winData) {
-        console.log(winData);
+        leftName.push(winData[0].summonerName)
+        console.log(leftName[0]);
+        buildLeft();
       });
     }
   });
 }
+// Api request left box end
+
+// function to populate left player box
+function buildLeft() {
+  console.log(leftName[0])
+  document.getElementById(sumName).innerHTML = leftName[0]
+  // nameLeft.innerHTML = "fuck you";
+};
+
+
 
 
 
 
 getAPILeft();
+
+// document.addEventListener('click',)
