776a7064d605d37ee96a662773bf828e2e088c00
KatyKedi <katykat5bros@gmail.com>
2022-04-04 11:57:26 -0700

    route through handlebars

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,16 +1,7 @@
-const path = require('path');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
-
-router.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/login.html'));
-});
-
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
+  res.render('homepage');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
