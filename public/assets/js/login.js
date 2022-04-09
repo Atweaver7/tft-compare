c859acfb784d02e405fcd536bfb95a6058b61a45
Sally <sallywing123@gmail.com>
2022-04-09 12:16:45 -0700

    added sign up functionality

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -9,4 +9,26 @@ function signIn(event){
 function signUp(event){
     event.preventDefault()
     // session logic
+    const emailInput =  document.querySelector('.sign-up-email').value; 
+    const passwordInput =  document.querySelector('.sign-up-password').value; 
+
+    const data = { email: emailInput, password: passwordInput };
+
+    fetch('/api/user/signup', {
+    method: 'POST', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+    if (data.id){
+        document.location.replace('/')
+    } else {console.log('fail to sign up')}
+    })
+    .catch((error) => {
+    console.error('Error:', error);
+    });
+
 }
