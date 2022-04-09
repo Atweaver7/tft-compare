9d113b92dbe83b1e1e68870eba33404872f8b0df
KatyKedi <katykat5bros@gmail.com>
2022-04-09 08:45:01 -0700

    begin login functionality

---
+++ public/assets/js/login.js
@@ -0,0 +1,12 @@
+const emailInput =  document.querySelector('.sign-in-email');
+const passwordInput = document.querySelector('.sign-in-password');
+
+function signIn(event){
+    event.preventDefault()
+    // session logic
+}
+
+function signUp(event){
+    event.preventDefault()
+    // session logic
+}
