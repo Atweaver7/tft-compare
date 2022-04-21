6e8601c73aad1553dce661453fec772d5378beaf
KatyKedi <katykat5bros@gmail.com>
2022-04-21 10:58:01 -0700

    fix user routes

--- routes/apiRoutes/userroutes.js
+++ routes/apiRoutes/userroutes.js
@@ -1,30 +1,68 @@
 const router = require('express').Router();
 const { User } = require("../../models");
 
-router.post('/signup', (req, res) => {
-    // Access our User model and run .findAll() method)
-    User.create({email: req.body.email,
-        password: req.body.password,})
-    
-    .then(dbUser => res.json(dbUser))
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] },
+    })
+    .then(dbUser => res.json(dbUser));
+})
+
+router.post('/', (req, res) => {
+    User.create({
+        email: req.body.email,
+        password: req.body.password,
+    })
+    .then(dbUserData => 
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.email = dbUserData.email;
+            req.session.loggedIn = true;
+        
+            res.json(dbUserData);
+        })
+    )
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
 });
 
-router.post('/login', (req,res) => {
+router.post('/login', (req, res) => {
     User.findOne({
         where: {
             email: req.body.email,
         }   
     }).then((dbUserData)=>{
         if (dbUserData) {
-            res.status(200).json(dbUserData)
+            const validPassword = dbUserData.checkPassword(req.body.password);
+            if (!validPassword) {
+                res.status(400).json({ message: 'Incorrect password!' });
+                return;
+            }
+            req.session.save(() => {
+                // declare session variables
+                req.session.user_id = dbUserData.id;
+                req.session.email = dbUserData.email;
+                req.session.loggedIn = true;
+        
+                res.json({ user: dbUserData, message: 'You are now logged in!' });
+            });
         } else {
             res.status(400).json({Message: "No User Found"})
         }
     }); 
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
 });
 
 module.exports = router;
